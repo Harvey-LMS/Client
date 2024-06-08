c79f446f98cb93bae5eebae3a25716ca49401f52
Dieglevel <dieglevel@gmail.com>
2024-06-08 17:08:31 +0700

    conflit

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,8 +3,8 @@ import { Metadata } from "next";
 
 
 export const metadata: Metadata = {
-   title: 'Harvey | Register',
-}
+  title: "Harvey | Register",
+};
 const RegisterPage = () => {
    return (
       <Register></Register>
