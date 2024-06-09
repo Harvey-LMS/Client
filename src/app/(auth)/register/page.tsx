2827d30b02db889a4afe95ede27941b325ff01ec
colnat <nguyentanloc1108@gmail.com>
2024-06-09 13:37:11 +0700

    call fakeAPI with mockAPI, format code InputOTP;LoginPage;RegisterPage

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,14 +1,11 @@
 import Register from "@/components/container/register";
 import { Metadata } from "next";
 
-
 export const metadata: Metadata = {
   title: "Harvey | Register",
 };
 const RegisterPage = () => {
-   return (
-      <Register></Register>
-   );
+  return <Register></Register>;
 };
 
 export default RegisterPage;
