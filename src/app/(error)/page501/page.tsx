05a3309a14dc5b98e418ad4866fde9814380a060
thienphu <phutot1111@gmail.com>
2024-06-22 17:26:14 +0700

    phu-them: hoan thien trang Error 501

--- src/app/(error)/page501/page.tsx
+++ src/app/(error)/page501/page.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image";
+import Error501 from "../../../assets/Error_501.svg"
+import {Button} from "@/components/ui/button";
+
 const Page = () => {
     return (
-        <div>
-            <h1>501</h1>
-            <p>Page not implemented</p>
+        <div className={"h-screen"}>
+            <div className={"flex flex-col items-center justify-center h-full"}>
+                <div className={"text-7xl text-gray-400 font-bold"}>NOT IMPLEMENT</div>
+                <Image
+                    src={Error501}
+                    alt={"Error 501"}
+                />
+                <div className={"text-2xl text-gray-500"}>Contact us: admin@gmail.com</div>
+                <Button className={"mt-5 bg-green-500 w-60 h-12 font-semibold text-1xl"}>
+                    GO BACK
+                </Button>
+            </div>
         </div>
     );
 }
