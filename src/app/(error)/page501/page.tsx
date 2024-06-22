618466e2eb42017cec3ead2215f4cc8289f63831
thienphu <phutot1111@gmail.com>
2024-06-22 17:43:15 +0700

    phu-capnhat: hoan thien trang Error 502 va responsive cho page 501 va 502

--- src/app/(error)/page501/page.tsx
+++ src/app/(error)/page501/page.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
-import Error501 from "../../../assets/Error_501.svg"
 import {Button} from "@/components/ui/button";
+import Error501 from "@/assets/Error_501.svg";
 
 const Page = () => {
     return (
         <div className={"h-screen"}>
             <div className={"flex flex-col items-center justify-center h-full"}>
-                <div className={"text-7xl text-gray-400 font-bold"}>NOT IMPLEMENT</div>
+                <div className={"text-4xl lg:text-7xl text-gray-400 font-bold"}>NOT IMPLEMENT</div>
                 <Image
+                    className={"self-center w-1/2 lg:w-1/3"}
                     src={Error501}
                     alt={"Error 501"}
                 />
-                <div className={"text-2xl text-gray-500"}>Contact us: admin@gmail.com</div>
-                <Button className={"mt-5 bg-green-500 w-60 h-12 font-semibold text-1xl"}>
+                <div className={"text-1xl lg:text-2xl text-gray-500"}>Contact us: admin@gmail.com</div>
+                <Button className={"mt-5 bg-green-500 w-40 lg:w-60 h-9 lg:h-12 font-semibold text-1xl"}>
                     GO BACK
                 </Button>
             </div>
