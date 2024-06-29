d7c1c3febd18edb1cc3921cb1ce435ad171f5286
colnat <nguyentanloc1108@gmail.com>
2024-06-29 15:24:56 +0700

    edit sidebar: toggleOpen

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,13 @@
-import NavbarComponent from "@/components/container/navbar-horizontal";
-import NavbarVerticalComponent from "@/components/container/navbar-vertical";
+import NavbarHorizontal from "@/components/container/navbar/navbar-horizontal";
+import NavbarVertical from "@/components/container/navbar/navbar-vertical";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-
-    return (
-        <div>
-                <NavbarComponent></NavbarComponent>
-                <NavbarVerticalComponent>{children}</NavbarVerticalComponent>
-        </div>
-
-    );
-}
+  return (
+    <div className="harvey bg-background text-foreground w-full">
+      <NavbarHorizontal></NavbarHorizontal>
+      <NavbarVertical>{children}</NavbarVertical>
+    </div>
+  );
+};
 
 export default DashboardLayout;
--- src/components/container/navbar/navbar-vertical.tsx
+++ src/components/container/navbar/navbar-vertical.tsx
@@ -9,28 +9,67 @@ import Courses from "@/assets/icon/courses-icon.svg";
 import Meeting from "@/assets/icon/meeting-icon.svg";
 import Wallet from "@/assets/icon/wallet-icon.svg";
 import Blog from "@/assets/icon/blog-icon.svg";
-import { Button } from "@nextui-org/react";
+import Menu from "@/assets/icon/menu-icon.svg";
+import Back from "@/assets/icon/back-icon.svg";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
-const NavbarVerticalComponent = ({ children }: { children: React.ReactNode }) => {
+const NavbarVertical = ({ children }: { children: React.ReactNode }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const toggleMenuBack = () => {
+    setIsMenuOpen(false);
+  };
   return (
     <div className="flex flex-row justify-center items-start">
-      <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800 w-64">
+      <Button className="md:hidden p-2" onClick={toggleMenu} variant="ghost">
+        <Image
+          alt={"menu"}
+          src={Menu}
+          className="shrink-0 aspect-square mr-2"
+          width={24}
+        />
+      </Button>
+      <div
+        className={`min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800 w-64 ${
+          isMenuOpen ? "block" : "hidden"
+        } md:block`}
+      >
         <div className="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
-          <div className="flex items-center justify-center h-14 border-b">
-            {/* <div>Sidebar Navigation By iAmine</div> */}
-          </div>
+          <div className="flex items-center justify-center h-14 border-b"></div>
           <div className="overflow-y-auto overflow-x-hidden flex-grow">
             <ul className="flex flex-col py-4 space-y-1">
               <li className="px-5">
                 <div className="flex flex-row items-center h-8">
                   <div className="text-sm font-light tracking-wide text-gray-500">
-                    <span>SERVICES</span>
+                    {isMenuOpen ? (
+                      <div className="flex flex-row justify-center items-center gap-2">
+                        <Button
+                          className="md:hidden p-2"
+                          onClick={toggleMenuBack}
+                          variant="ghost"
+                        >
+                          <Image
+                            alt={"back"}
+                            src={Back}
+                            className="shrink-0 aspect-square mr-2"
+                            width={16}
+                          />
+                        </Button>
+                        <span>SERVICES</span>
+                      </div>
+                    ) : (
+                      <span>SERVICES</span>
+                    )}
                   </div>
                 </div>
               </li>
 
-              <li className="ml-6">
-                <Link href={""}>
+              <li className="ml-6 hover:bg-primary hover:rounded-full hover:text-white hover:w-[80%]">
+                <Link href={""} className="">
                   <div className="flex gap-2.5 m-4 text-base tracking-normal leading-6 whitespace-nowrap text-zinc-700 text-opacity-90">
                     <Image
                       alt={"msg"}
@@ -43,7 +82,7 @@ const NavbarVerticalComponent = ({ children }: { children: React.ReactNode }) =>
                 </Link>
               </li>
 
-              <li className="ml-6">
+              <li className="ml-6 hover:bg-primary hover:rounded-full hover:text-white hover:w-[80%]">
                 <Link href={""}>
                   <div className="flex gap-2.5 m-4 text-base tracking-normal leading-6 whitespace-nowrap text-zinc-700 text-opacity-90">
                     <Image
@@ -57,7 +96,7 @@ const NavbarVerticalComponent = ({ children }: { children: React.ReactNode }) =>
                 </Link>
               </li>
 
-              <li className="ml-6">
+              <li className="ml-6 hover:bg-primary hover:rounded-full hover:text-white hover:w-[80%]">
                 <Link href={""}>
                   <div className="flex w-full gap-2.5 m-4 text-base tracking-normal leading-6 whitespace-nowrap text-zinc-700 text-opacity-90">
                     <Image
@@ -71,8 +110,8 @@ const NavbarVerticalComponent = ({ children }: { children: React.ReactNode }) =>
                 </Link>
               </li>
 
-              <li className="ml-6">
-                <Link href={""}>
+              <li className="ml-6 hover:bg-primary hover:rounded-full hover:text-white hover:w-[80%]">
+                <Link href={""} className="">
                   <div className="flex gap-2.5 m-4 text-base tracking-normal leading-6 whitespace-nowrap text-zinc-700 text-opacity-90">
                     <Image
                       alt={"courses"}
@@ -85,7 +124,7 @@ const NavbarVerticalComponent = ({ children }: { children: React.ReactNode }) =>
                 </Link>
               </li>
 
-              <li className="ml-6">
+              <li className="ml-6 hover:bg-primary hover:rounded-full hover:text-white hover:w-[80%]">
                 <Link href={""}>
                   <div className="flex gap-2.5 m-4 text-base tracking-normal leading-6 whitespace-nowrap text-zinc-700 text-opacity-90">
                     <Image
@@ -99,7 +138,7 @@ const NavbarVerticalComponent = ({ children }: { children: React.ReactNode }) =>
                 </Link>
               </li>
 
-              <li className="ml-6">
+              <li className="ml-6 hover:bg-primary hover:rounded-full hover:text-white hover:w-[80%]">
                 <Link href={""}>
                   <div className="flex gap-2.5 m-4 text-base tracking-normal leading-6 whitespace-nowrap text-zinc-700 text-opacity-90">
                     <Image
@@ -116,12 +155,9 @@ const NavbarVerticalComponent = ({ children }: { children: React.ReactNode }) =>
           </div>
         </div>
       </div>
-      <div className="w-full">
-        {children}
-      </div>
-
+      <div className="w-full">{children}</div>
     </div>
   );
 };
 
-export default NavbarVerticalComponent;
+export default NavbarVertical;
